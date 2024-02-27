6e6feb17c4bbfbaaefa33fe840a53b8f5ff4bb7f
Leon Hillmann <l.hillmann@tue.nl>
2024-02-27 15:44:35 +0100

    Feat: TOML simulation configuration

--- example_run.sh
+++ example_run.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-python -m cc3d.player5 -i example/simulation.cc3d -o example/output
+python -m cc3d.player5 -i simulation/simulation.cc3d -o example_output
--- simulation/Simulation/compartmentalised_nucleus.py
+++ simulation/Simulation/compartmentalised_nucleus.py
@@ -1,22 +1,27 @@
+import toml
+from typing import TypedDict, cast
+
 from cc3dslib.nucleus import NucleusCompartmentCell, NucleusCompartmentCellParams
 from cc3dslib.filter import CompartmentFilter, RandomFractionFilter
 from cc3dslib import ActiveSwimmer, ActiveSwimmerParams
 from cc3dslib.simulation import ConfigBuilder, PottsParams
+from cc3dslib.analysis import COMTracker
 
-mode = "multi"
-contact_internal = 0.5
+
+config = toml.load("config.toml")
+
 
 sim_params = PottsParams(
-    dimensions=(200, 200, 1),
-    steps=10_000,
+    dimensions=(config["sim"]["box_size"], config["sim"]["box_size"], 1),
+    steps=config["sim"]["steps"],
     neighborOrder=1,
 )
 
 # nucleus
 nuc_params = NucleusCompartmentCellParams(
-    diameter=40,
-    box=(80, 80, 120, 120) if mode == "single" else (0, 0, *sim_params.dimensions[:2]),
-    nucleus_size_ratio=0.56,
+    diameter=config["cell"]["diameter"],
+    box=(0, 0, *sim_params.dimensions[:2]),
+    nucleus_size_ratio=config["cell"]["nucleus_size_ratio"],
     nuc_lambda_volume=6,
     cyto_lambda_volume=3,
     neighbour_order_volume=16,
@@ -28,7 +33,7 @@ nuc_params.contact_energy[("Cytoplasm", "Cytoplasm")] = 1.0
 nuc_params.contact_energy[("Nucleus", "Medium")] = 10.0
 nuc_params.contact_energy[("Nucleus", "Cytoplasm")] = 10.0
 nuc_params.contact_energy[("Nucleus", "Nucleus")] = 100.0
-nuc_params.contact_internal[("Cytoplasm", "Nucleus")] = contact_internal
+nuc_params.contact_internal[("Cytoplasm", "Nucleus")] = 0.5
 nuc_steppable = NucleusCompartmentCell(params=nuc_params)
 
 # active force
@@ -52,14 +57,46 @@ class RandomFilter(RandomFractionFilter):
         np.savetxt(self.path, self.indices, fmt="%d")
 
 
-active_filter = RandomFilter(cell_filter, 1.0, path="example/output/ids_motile.txt")
+active_filter = RandomFilter(cell_filter, fraction=config["sim"]["active_fraction"])
 
 active_params = ActiveSwimmerParams(
     filter=active_filter,
     d_theta=0.1,
-    force_magnitude=200.0,
+    force_magnitude=config["cell"]["propensity"],
 )
 active_steppable = ActiveSwimmer(params=active_params)
+
+
+def com_filter_fn(cells):
+    """This function is used to filter cells for COMTracker.
+
+    It returns a list of lists of cells. For each compartment, the outer list contains a
+    list of all the cells within a compartment (cytoplasm and nucleus). Following this,
+    there are then two lists, one for the cytoplasm and one for the nucleus, each
+    containing a single cell. Thus, for each cell, the result of this function looks as
+    follows:
+
+    [[CYTOPLASM, NUCLEUS], [CYTOPLASM], [NUCLEUS]]
+
+    The resulting h5 py tables file will have the following structure:
+
+    (t, 0) -> COM_COMPARTMENT
+    (t, 1) -> COM_CYTOPLASM
+    (t, 2) -> COM_NUCLEUS
+    """
+    import itertools
+
+    return itertools.chain(
+        *[[cell] + [[c] for c in sorted(cell, key=lambda c: c.type)] for cell in cells]
+    )
+
+
+com_tracker = COMTracker(
+    "com.h5",
+    cell_filter.transform(com_filter_fn),
+    frequency=100,
+)
+
 
 (
     ConfigBuilder()
@@ -69,6 +106,7 @@ active_steppable = ActiveSwimmer(params=active_params)
     .add(cell_filter)
     .add(active_filter)
     .add(active_steppable)
+    .add(com_tracker)
     .setup()
     .run()
 )
